7431a3de44b6d75acf3b56dfce091c741e505ae8
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-11-02 21:25:40 +0000

    basic switch statement

---
+++ switch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int value = 3;
+
+    switch (value) {
+        case 1:
+            printf("1\n");
+            break;
+        case 2:
+            printf("2\n");
+            break;
+        default:
+            printf("0\n");
+    }
+}
